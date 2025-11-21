b422022d1c3c2b732bb3b63009def5a380cbb2f3
sbalusu <saikrishnabalusu97@gmail.com>
2025-11-21 14:26:17 +0530

    cartReducerImplemented

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,16 +1,25 @@
 
 import { useParams } from 'react-router-dom';
-import  store  from "../Store/store.js";
 import { addToCart, cartSlice, removeFromCart } from '../Store/CartSlice.js';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function Details() {
 
   let {id} = useParams();  // useParams returns object with key id and the value is string not int ex. {id:"2"}
   const productId = parseInt(id); // parsing the string to an integer
+  const dispatch = useDispatch();
   const {products} = useSelector((state)=>state.products)  // redux hook to select & subscribe the a state from redux store
   const [detailedProduct] = products.filter((product)=>product.id === productId)
+  const {value:cartItem} = useSelector((state)=>state.cart) // here value:cartItem means i'm aliasing the value as cartItem to increase readability
+
+  function handleAddtoCart(){
+    
+    cartItem.filter(item=>item.id==detailedProduct.id).length > 0 ?
+       console.log(`${detailedProduct.title} product in cart`):
+        dispatch(addToCart({product:detailedProduct}))
+    
+  }
 
   return (
     <div className='w-[760px] h-[760px] mx-auto my-auto flex justify-between items-center select-none'>
@@ -23,7 +32,7 @@ function Details() {
                 <h2 className={detailedProduct.rating.rate > 4 ? 'bg-green-300 px-4 py-1 text-white font-bold rounded-lg':'bg-red-300 px-4 py-1 text-white font-bold rounded-lg'}>{detailedProduct.rating.rate}{'\u2605'}</h2>
             </div>
             <div className="flex justify-between items-center gap-2 mt-4">
-                <button className='px-3 py-2 bg-black text-white text-xl font-semibold rounded-lg'>  Add to Cart </button>
+                <button className='px-3 py-2 bg-black text-white text-xl font-semibold rounded-lg' onClick={handleAddtoCart}>  Add to Cart </button>
                 <button className='px-3 py-2 bg-black text-white text-xl font-semibold rounded-lg'> Add to WishList </button>
             </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,29 @@ import meeshologo from '../assets/meeshologo.svg'
 import cartlogo from '../assets/CartIcon.svg'
 import profileIcon from '../assets/ProfileIcon.svg'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function Header({...props}) {
+  const {value:cartItems}=useSelector((state)=>state.cart)
   return (
-    <div className=" text-neutral-600 flex gap-4 justify-between p-3 items-center border-b-neutral-900 border select-none">
+    <div className="w-screen h-16 text-neutral-600 flex gap-4 justify-between bg-white p-2 items-center border-b-neutral-900 border select-none sticky top-0 z-10">
         <div className='w-full h-full flex gap-4 '>
           <Link to="/" >
-            <div className="  w-44 h-full">
+            <div className="w-64 h-full ">
                 <img src={meeshologo} alt="Logo"  className='w-full h-full object-cover mix-blend-multiply'/>
             </div>
             </Link>
-            <input type="search" className='w-72  text-neutral-500 outline-none px-2 overflow-x-auto rounded-lg text-xl border border-neutral-400 ' placeholder='Search products....'/>
+            <input type="search" className='w-96  text-neutral-500 outline-none px-2 overflow-x-auto rounded-lg text-xl border border-neutral-400 ' placeholder='Search products....'/>
         </div>
-         <div className=" p-2 w-14 h-full flex justify-center gap-4">
-            <img src={cartlogo} alt="Logo"  className='w-full h-full object-cover mix-blend-multiply'/>
-            <img src={profileIcon} alt="profileIcon" className='w-full h-full object-cover mix-blend-multiply' />
+         <div className="w-32 h-full flex justify-center gap-6 pr-4">
+            <div className='w-full h-full relative' >
+              <img src={cartlogo} alt="Logo"  className='w-full h-full object-cover mix-blend-multiply'/>
+              <div className='absolute top-0 -right-1 w-5 h-5 bg-red-600 text-white text-sm font-medium rounded-full text-center '>{cartItems.length}</div>
+            </div>
+            <div className='w-full h-full ' >
+              <img src={profileIcon} alt="profileIcon" className='w-full h-full object-cover mix-blend-multiply' />
+            </div>
+            
         </div>
         
     </div>
